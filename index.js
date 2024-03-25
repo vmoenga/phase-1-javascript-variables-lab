d93c326b97c1f615962fe7f8fd1569404ef85e0a
Victor <vmoenga36@gmail.com>
2024-03-25 17:06:05 +0300

    javascript variables

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file!
+// index.js
+
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
+
+
+module.exports.companyName = companyName;
+module.exports.mostProfitableNeighborhood = mostProfitableNeighborhood;
+module.exports.companyCeo = companyCeo;
